5dba5f318ae4087f6fad2e98043de21ab8bb9966
Adam Kelly <adam@cthulahoops.org>
2020-05-29 21:13:47 +0100

    Small javascript changes.
    * Changes colors.
    * Numbers cells from 1.
    * Replace width/height with board_size.
    * Star points for 9x9.
    * Example stones in more real places.

--- www/index.js
+++ www/index.js
@@ -2,40 +2,41 @@
 
 //wasm.greet();
 const CELL_SIZE = 35; // px
-const BOARD_COLOR = "#bdb38e";
-const GRID_COLOR = "#CCCCCC";
+const BOARD_COLOR = "#ddb34e";
+const GRID_COLOR = "#333333";
 const WHITE = "#FFFFFF";
 const BLACK = "#000000";
 
-const width = 19;
-const height = 19;
+const board_size = 19;
+
+const cell_transform = (cell) => {
+  return (cell) * (CELL_SIZE + 1)
+};
 
 const canvas = document.getElementById("goban");
-canvas.height = (CELL_SIZE + 1) * (height+1) + 1;
-canvas.width = (CELL_SIZE + 1) * (width+1) + 1;
+canvas.height = cell_transform(board_size + 1);
+canvas.width = cell_transform(board_size + 1);
 
 const ctx = canvas.getContext('2d');
 
-const cell_transform = (cell) => {
-  return (cell + 1) * (CELL_SIZE + 1)
-};
 
 const drawGrid = () => {
   ctx.beginPath();
   ctx.strokeStyle = GRID_COLOR;
 
   ctx.fillStyle = BOARD_COLOR;
-  ctx.fillRect(0,0,canvas.width,canvas.height);
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
   // Vertical lines.
-  for (let i = 0; i < width; i++) {
-    ctx.moveTo(cell_transform(i) + 1, cell_transform(0));
-    ctx.lineTo(cell_transform(i) + 1, cell_transform(height - 1) + 1);
+  for (let i = 1; i <= board_size; i++) {
+    ctx.moveTo(cell_transform(i) + 1, cell_transform(1));
+    ctx.lineTo(cell_transform(i) + 1, cell_transform(board_size) + 1);
   }
 
   // Horizontal lines.
-  for (let j = 0; j < height; j++) {
-    ctx.moveTo(cell_transform(0),           cell_transform(j) + 1);
-    ctx.lineTo(cell_transform(width - 1) + 1, cell_transform(j) + 1);
+  for (let j = 1; j <= board_size; j++) {
+    ctx.moveTo(cell_transform(1),         cell_transform(j) + 1);
+    ctx.lineTo(cell_transform(board_size) + 1, cell_transform(j) + 1);
   }
 
   ctx.stroke();
@@ -53,27 +54,34 @@ const drawDot = (x, y) => {
     false);
   ctx.fill();
 }
+
 const drawBoard = () => {
-  if (height === 13) {
+  if (board_size === 9) {
     drawDot(3,3);
-    drawDot(3,9);
-    drawDot(9,3);
-    drawDot(9,9);
-    drawDot(6,6);
-  } else if (height === 19) {
-    drawDot(3,3);
-    drawDot(3,15);
-    drawDot(15,3);
-    drawDot(15,15);
-    drawDot(9,9);
-    drawDot(9,3);
-    drawDot(3,9);
-    drawDot(15,9);
-    drawDot(9,15);
+    drawDot(5,5);
+    drawDot(7,7);
+    drawDot(3,7);
+    drawDot(7,3);
+  } else if (board_size === 13) {
+    drawDot(4,4);
+    drawDot(4,10);
+    drawDot(10,4);
+    drawDot(10,10);
+    drawDot(7,7);
+  } else if (board_size === 19) {
+    drawDot(4,4);
+    drawDot(4,16);
+    drawDot(16,4);
+    drawDot(16,16);
+    drawDot(10,10);
+    drawDot(10,4);
+    drawDot(4,10);
+    drawDot(16,10);
+    drawDot(10,16);
   }
 };
 
-const drawStone  = (x, y, color) => {
+const drawStone = (x, y, color) => {
   ctx.beginPath();
   ctx.fillStyle = color;
   ctx.arc(
@@ -88,6 +96,8 @@ const drawStone  = (x, y, color) => {
 
 drawGrid();
 drawBoard();
-drawStone(1,1, BLACK);
-drawStone(8,8, BLACK);
-drawStone(3,4, WHITE);
+drawStone(3,4, BLACK);
+drawStone(5,3, WHITE);
+drawStone(7,3, BLACK);
+drawStone(5,5, WHITE);
+drawStone(4,6, BLACK);
