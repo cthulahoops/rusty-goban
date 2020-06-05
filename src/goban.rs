98d6666915310b9c68c6ccd35e3627eaa6b56125
Adam Kelly <adam@cthulahoops.org>
2020-06-05 11:13:30 +0100

    Format rust code with cargo fmt.

--- src/goban.rs
+++ src/goban.rs
@@ -21,115 +21,129 @@ pub struct Position {
 }
 
 pub struct Board {
-  pub map: HashMap<Position, Stone>,
-  pub size: i32,
-  pub next_player: Stone
+    pub map: HashMap<Position, Stone>,
+    pub size: i32,
+    pub next_player: Stone,
 }
 
 impl Board {
-  pub fn new(size : i32) -> Self {
-    Board {
-      map: HashMap::new(),
-      size,
-      next_player: Black,
-    }
-  }
-
-  fn has_liberties(&self, position: Position) -> bool {
-    let player = self.map.get(&position).unwrap();
-    let mut visited : HashSet<Position> = HashSet::new();
-    let mut queue = vec![position];
-      
-    while let Some(next) = queue.pop() {
-      for adj_position in self.adjacent(next) {
-        if visited.contains(&adj_position) {
-          continue;
-        } else {
-          visited.insert(adj_position);
+    pub fn new(size: i32) -> Self {
+        Board {
+            map: HashMap::new(),
+            size,
+            next_player: Black,
         }
-        match self.map.get(&adj_position) {
-          None => { return true; }
-          Some(adj_player) => {
-            if adj_player == player {
-              queue.push(adj_position);
+    }
+
+    fn has_liberties(&self, position: Position) -> bool {
+        let player = self.map.get(&position).unwrap();
+        let mut visited: HashSet<Position> = HashSet::new();
+        let mut queue = vec![position];
+
+        while let Some(next) = queue.pop() {
+            for adj_position in self.adjacent(next) {
+                if visited.contains(&adj_position) {
+                    continue;
+                } else {
+                    visited.insert(adj_position);
+                }
+                match self.map.get(&adj_position) {
+                    None => {
+                        return true;
+                    }
+                    Some(adj_player) => {
+                        if adj_player == player {
+                            queue.push(adj_position);
+                        }
+                    }
+                }
             }
-          }
         }
-      }
+        return false;
     }
-    return false;
-  }
 
-  fn has_stone(&self, position : Position, stone: Stone) -> bool {
-    if let Some(adj_stone) = self.map.get(&position) {
-      return *adj_stone == stone 
+    fn has_stone(&self, position: Position, stone: Stone) -> bool {
+        if let Some(adj_stone) = self.map.get(&position) {
+            return *adj_stone == stone;
+        }
+        false
     }
-    false
-  }
 
-  fn remove_group(&mut self, position : Position) -> () {
-    let origin_stone = self.map.remove(&position).unwrap();
-    for adj_position in self.adjacent(position) {
-      if self.has_stone(adj_position, origin_stone) {
-        self.remove_group(adj_position);
-      }
+    fn remove_group(&mut self, position: Position) -> () {
+        let origin_stone = self.map.remove(&position).unwrap();
+        for adj_position in self.adjacent(position) {
+            if self.has_stone(adj_position, origin_stone) {
+                self.remove_group(adj_position);
+            }
+        }
     }
-  }
 
-  pub fn play_stone(&mut self, position : Position) -> Result<(), String> {
-    let stone = self.next_player;
-      // Check for ko!
+    pub fn play_stone(&mut self, position: Position) -> Result<(), String> {
+        let stone = self.next_player;
+        // Check for ko!
 
-      if !self.is_on_board(&position) {
-        return Err(format!("Play on the board {:?})", position).to_string());
-      }
+        if !self.is_on_board(&position) {
+            return Err(format!("Play on the board {:?})", position).to_string());
+        }
 
-      if self.map.contains_key(&position) {
-        return Err("Already occupied.".to_string());
-      }
-      
-      self.map.insert(position, stone);
-  
-      for adj_position in self.adjacent(position) {
-          if self.has_stone(adj_position, other_player(stone)) {
-              println!("{:?}", adj_position);
-              if !self.has_liberties(adj_position) {
-                  self.remove_group(adj_position);
-              }
-          } else {
-              continue
-          }
-      } 
-      if !self.has_liberties(position) {
-          self.map.remove(&position);
-          return Err("Illegal move - no liberties".to_string());
-      }
+        if self.map.contains_key(&position) {
+            return Err("Already occupied.".to_string());
+        }
 
-      self.next_player = other_player(self.next_player);
-      Ok(())
-  }
+        self.map.insert(position, stone);
 
-  fn is_on_board(&self, pos: &Position) -> bool {
-    self.size >= pos.x && pos.x >= 1 && self.size >= pos.y && 1 <= pos.y
-  }
+        for adj_position in self.adjacent(position) {
+            if self.has_stone(adj_position, other_player(stone)) {
+                println!("{:?}", adj_position);
+                if !self.has_liberties(adj_position) {
+                    self.remove_group(adj_position);
+                }
+            } else {
+                continue;
+            }
+        }
+        if !self.has_liberties(position) {
+            self.map.remove(&position);
+            return Err("Illegal move - no liberties".to_string());
+        }
 
-  fn adjacent (&self, position : Position) -> Vec<Position> {
-    let adj_positions = vec! [
-      Position {x: position.x + 1, y: position.y},
-      Position {x: position.x - 1, y: position.y},
-      Position {x: position.x, y: position.y + 1},
-      Position {x: position.x, y: position.y - 1},
-    ];
-    adj_positions
-      .into_iter()
-      .filter(|x| self.is_on_board(x))
-      .collect()
-  }
+        self.next_player = other_player(self.next_player);
+        Ok(())
+    }
+
+    fn is_on_board(&self, pos: &Position) -> bool {
+        self.size >= pos.x && pos.x >= 1 && self.size >= pos.y && 1 <= pos.y
+    }
+
+    fn adjacent(&self, position: Position) -> Vec<Position> {
+        let adj_positions = vec![
+            Position {
+                x: position.x + 1,
+                y: position.y,
+            },
+            Position {
+                x: position.x - 1,
+                y: position.y,
+            },
+            Position {
+                x: position.x,
+                y: position.y + 1,
+            },
+            Position {
+                x: position.x,
+                y: position.y - 1,
+            },
+        ];
+        adj_positions
+            .into_iter()
+            .filter(|x| self.is_on_board(x))
+            .collect()
+    }
 }
 
 fn other_player(stone: Stone) -> Stone {
-  match stone {
-    Black => White,
-    White => Black,
-  }
+    match stone {
+        Black => White,
+        White => Black,
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-mod utils;
 mod goban;
+mod utils;
 
-use wasm_bindgen::prelude::*;
 use goban::{Board, Position, Stone};
 use utils::set_panic_hook;
+use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -20,8 +20,8 @@ pub struct JsBoard {
 impl Stone {
     pub fn to_str(&self) -> &str {
         match &self {
-          Stone::White => "white",
-          Stone::Black => "black",
+            Stone::White => "white",
+            Stone::Black => "black",
         }
     }
 }
@@ -29,30 +29,34 @@ impl Stone {
 // Javascript specific methods for the board.
 #[wasm_bindgen]
 impl JsBoard {
-  pub fn new(size: i32) -> Self {
-    set_panic_hook();
-    JsBoard {
-      board: Board::new(size),
-      size: size,
+    pub fn new(size: i32) -> Self {
+        set_panic_hook();
+        JsBoard {
+            board: Board::new(size),
+            size: size,
+        }
     }
-  }
 
-  pub fn next_player(&self) -> JsValue {
-    JsValue::from(self.board.next_player.to_str())
-  }
-
-  pub fn play_stone(&mut self, x : i32, y: i32) -> Result<(), JsValue> {
-    self.board.play_stone(Position { x, y }).map_err(JsValue::from)
-  }
-
-  pub fn draw_stones(&self, f : &js_sys::Function) -> () {
-    for (position, stone) in self.board.map.iter() {
-      let this = JsValue::NULL;
-      f.call3(
-        &this, 
-        &JsValue::from(position.x), 
-        &JsValue::from(position.y), 
-        &JsValue::from(stone.to_str())).unwrap();
+    pub fn next_player(&self) -> JsValue {
+        JsValue::from(self.board.next_player.to_str())
     }
-  }
+
+    pub fn play_stone(&mut self, x: i32, y: i32) -> Result<(), JsValue> {
+        self.board
+            .play_stone(Position { x, y })
+            .map_err(JsValue::from)
+    }
+
+    pub fn draw_stones(&self, f: &js_sys::Function) -> () {
+        for (position, stone) in self.board.map.iter() {
+            let this = JsValue::NULL;
+            f.call3(
+                &this,
+                &JsValue::from(position.x),
+                &JsValue::from(position.y),
+                &JsValue::from(stone.to_str()),
+            )
+            .unwrap();
+        }
+    }
 }
